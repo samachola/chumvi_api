64e808b86d266c86e7389002ae0df137ae3bbe5e
sanachola <sam.achola@live.com>
2017-10-28 18:38:10 +0300

    UPADATE: added user, category and recipe models

--- app.py
+++ app.py
@@ -14,7 +14,17 @@ class User(db.Model):
     admin = db.Column(db.Boolean)
     password = db.Column(db.String(80))
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(50))
 
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    ingredients = db.Column(db.String)
+    steps = db.Column(db.String)
+    category_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
 
 if __name__ == '__main__':
     app.run()
